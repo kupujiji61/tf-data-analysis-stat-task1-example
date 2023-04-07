b5f2eda3d1830d84be40e8291d352ee08c3c983b
kupujiji61 <130174574+kupujiji61@users.noreply.github.com>
2023-04-07 22:42:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,5 +5,4 @@ import numpy as np
 chat_id = 778100570 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    x = x - 443
     return np.log(np.mean(x)) - np.std(x)**2 / 2
