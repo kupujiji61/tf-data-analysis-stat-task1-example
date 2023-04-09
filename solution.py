d2211700b3921da710212df61ae455df8b6d7880
kupujiji61 <130174574+kupujiji61@users.noreply.github.com>
2023-04-09 19:21:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,5 +5,4 @@ import numpy as np
 chat_id = 778100570 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    x = np.log(x + 443)
-    return np.mean(x)
+    return np.log(x - 443).mean()
